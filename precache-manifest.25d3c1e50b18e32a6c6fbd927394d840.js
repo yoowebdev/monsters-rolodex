9fc87cedf2c179b5abe7e959ba54c99e5453a50f
yoowebdev <yoowebdev@gmail.com>
2019-09-27 01:06:40 -0500

    Updates

---
+++ precache-manifest.25d3c1e50b18e32a6c6fbd927394d840.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9d4d80d233f24692586a6cd43e83c7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8057d01ee84691cfd192",
+    "url": "/monsters-rolodex/static/css/main.38be2a58.chunk.css"
+  },
+  {
+    "revision": "d13a353a0fe4879286ec",
+    "url": "/monsters-rolodex/static/js/2.d395d0d5.chunk.js"
+  },
+  {
+    "revision": "8057d01ee84691cfd192",
+    "url": "/monsters-rolodex/static/js/main.4c7f3125.chunk.js"
+  },
+  {
+    "revision": "f9d70e3af54ddae19106",
+    "url": "/monsters-rolodex/static/js/runtime-main.e20afc0a.js"
+  }
+]);
